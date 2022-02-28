561e739346f2ce52532954032fbe38d58e360482
Andrew Washburn <alwashburn91@gmail.com>
2022-02-28 12:16:42 -0800

    add sound after each goal arrival

--- src/nav_goals_node.cpp
+++ src/nav_goals_node.cpp
@@ -40,20 +40,20 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "simple_navigation_goals");
     ros::NodeHandle nh;
     // tell action client spin a thread by default
-    // MoveBaseClient ac("move_base", true);
+    MoveBaseClient ac("move_base", true);
     sound_play::SoundClient sc;
     // wait for action server to come up
-    // while(!ac.waitForServer(ros::Duration(5.0))) {
-    //     ROS_INFO("Waiting for the move_base action server to come up");
-    // }
+    while(!ac.waitForServer(ros::Duration(5.0))) {
+        ROS_INFO("Waiting for the move_base action server to come up");
+    }
     
     sleepok(1, nh);
     // Initialize move_base goal message
     move_base_msgs::MoveBaseGoal goal;
     goal.target_pose.header.frame_id = "map";
     int i = 0;
-    // std::vector<place_t> waypoints;
-    // TODO use a vector for waypoint navigation
+
+    // std::vector<place_t> waypoints;  // TODO use a vector for waypoint navigation
     // map hallway goal posts
     // (47.392, 66.779, 1.566)  E-door 
     // (47.923, 56.401, -1.577) Student door
@@ -61,27 +61,27 @@ int main(int argc, char** argv)
     // (53.455, 51.290, -0.106) P3 door
     // (25.321, 56.717, 1.627)  Assistance desk
     // (37.307, 38.004, -3.097) Front of other help desk
-    /** int num_goals = 3;
-    // float x[num_goals] = {36.469, 47.323, 47.392};
-    // float y[num_goals] = {53.735, 56.401, 66.779};
-    // float yaw[num_goals] = {-0.016, 1.577, 1.566};
+    int num_goals = 3;
+    float x[num_goals] = {36.469, 47.323, 47.392};
+    float y[num_goals] = {53.735, 56.401, 66.779};
+    float yaw[num_goals] = {-0.016, 1.577, 1.566};
     string places[num_goals] = {
         "Hallway",
         "Student Worker room",
         "Emergency Exit door"
-    }; */
+    }; 
 
-    int num_goals = 5;
-    float x[num_goals] = {37.307, 25.321, 53.455, 47.323, 47.392};
-    float y[num_goals] = {38.004, 56.717, 52.290, 56.401, 66.779};
-    float yaw[num_goals] = {-3.097, 1.627, -0.106, 1.577, 1.566};
-    string places[num_goals] = {
-        "Front help desk",
-        "Assistance Desk",
-        "P3 Door",
-        "Student Worker room",
-        "Emergency Exit door"
-    };
+    // int num_goals = 5;
+    // float x[num_goals] = {37.307, 25.321, 53.455, 47.323, 47.392};
+    // float y[num_goals] = {38.004, 56.717, 52.290, 56.401, 66.779};
+    // float yaw[num_goals] = {-3.097, 1.627, -0.106, 1.577, 1.566};
+    // string places[num_goals] = {
+    //     "Front help desk",
+    //     "Assistance Desk",
+    //     "P3 Door",
+    //     "Student Worker room",
+    //     "Emergency Exit door"
+    // };
 
     while(nh.ok()) {
         goal.target_pose.header.stamp = ros::Time::now();
@@ -89,23 +89,26 @@ int main(int argc, char** argv)
         goal.target_pose.pose.position.y = y[i];
         goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw[i]);
 
-        string wav = base_sound_path + phrases[i];
-        sc.playWave(wav);
-        ROS_INFO("Playing [%d] %s", i, wav.c_str());
-        sleepok(4, nh);
-        sc.stopWave(wav);
+        ROS_INFO("Sending goal to '%s'", places[i].c_str());
+        ac.sendGoal(goal);
 
-        // ROS_INFO("Sending goal to '%s'", places[i].c_str());
-        // ac.sendGoal(goal);
+        ac.waitForResult();
 
-        // ac.waitForResult();
+        if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+            string stop_arrive = base_sound_path + phrases[1];
+            string delivered = base_sound_path + phrases[2];
 
-        // if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-            
-        // }
-        // else {
-        //     ROS_WARN("Could not reach goal.");
-        // }
+            sc.playWave(stop_arrive);
+            ROS_INFO("Playing stop_arrive");
+            sleepok(6, nh);
+            sc.stopWave(stop_arrive);
+            ROS_INFO("Press enter to continue to next goal.");
+            std::cin;
+            sc.playWave(delivered);
+        }
+        else {
+            ROS_WARN("Could not reach goal.");
+        }
         i = (++i) % num_goals;  // increment to next goal.
     }
     
